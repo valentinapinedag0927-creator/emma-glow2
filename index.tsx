2cedc1323f3791c4cb4051a3205c24696233ea3f
valentinapinedag0927-creator <valentina.pinedag0927@gmail.com>
2026-04-05 10:20:02 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Este mensaje aparecerá en la consola si el archivo carga
-console.log("¡Hola! El motor de Emma Glow ha despertado.");
-
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  alert("No encontré el div 'root'. Revisa el HTML.");
+  root.render(<App />);
 }
